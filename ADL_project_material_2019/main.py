5abd2f0dd1615b68df1569881a756f4b0f151e1a
James Keen <24jkeen@gmail.com>
2019-01-11 11:14:50 +0000

    Force the split to contain each label at least once

--- ADL_project_material_2019/main.py
+++ ADL_project_material_2019/main.py
@@ -45,21 +45,31 @@ def sample(data, data_a):
 
     groups   = [x[0] for x in groups_all][700:]
     groups_a = [x[1] for x in groups_all][:700]
+    ## checks
+
+    while len(set(np.row_stack( np.array([row["labels"].values for row in groups])).flatten())) != 10 and len(set(np.row_stack( np.array([row["labels"].values for row in groups_a])).flatten())) != 10:
+        print("Must reshuffle")
+        random.shuffle(groups_all)
+        groups   = [x[0] for x in groups_all][700:]
+        groups_a = [x[1] for x in groups_all][:700]
 
     # random.shuffle(groups)
     # random.shuffle(groups_a)
-
+    print("I")
     dataset = pd.concat(groups).reset_index(drop=True)
     dataset_a = pd.concat(groups_a).reset_index(drop=True)
 
+    print("AM")
     trainBatch  = list(np.row_stack(dataset_a["data"].values))
     trainBatch  = np.array(list(map(utils.melspectrogram, trainBatch)), dtype=np.float32)
 
+    print("SO")
     trainLabels = np.array(pd.get_dummies(dataset_a["labels"]).values, dtype=np.float32)
 
     testBatch   = np.row_stack(dataset["data"].values)
     testBatch  = np.array(list(map(utils.melspectrogram, testBatch)), dtype=np.float32)
 
+    print("BORED")
     testLabels  = np.array(pd.get_dummies(dataset["labels"]).values, dtype=np.float32)
 
     return trainBatch, testBatch, trainLabels, testLabels
